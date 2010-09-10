af19bf3a1f9b0dbee8d1ea7ad3503b0387821bdb
Tim Trefren <ttrefren@gmail.com>
2010-09-10 11:40:24 -0700

    naming conflict resolved

--- iptables.py
+++ iptables.py
@@ -1,15 +1,19 @@
 from django.template import Context, Template
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 def generate_iptables(filename, ips):
     """
         Generate iptables files from templates.
     """
-    path = os.path.dirname(__file__)
-    f = open('%s/templates/%s' % (path, filename))
+    f = open('templates/%s' % filename)
     f = ''.join([line for line in f])
     t = Template(f)    
     c = Context({ 'servers': ips })
     rendered = t.render(c)
 
-    out = open('%s/rendered/%s' % (path, filename), 'w')
+    out = open('rendered/%s' % filename, 'w')
     out.write(rendered)
+
+if __name__ == "__main__":
+    generate_iptables('iptables.int.rules', ['1.1.1.1', '2.2.2.2'])
