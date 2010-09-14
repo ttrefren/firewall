614475697c6151c6b81fc1ee76c6f3b504ab8cf8
Tim Trefren <ttrefren@gmail.com>
2010-09-14 11:47:55 -0700

    Add support for txt file of ips

--- iptables.py
+++ iptables.py
@@ -2,10 +2,17 @@ from django.template import Context, Template
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+def generate_iptables_from_file(output_file, input_file):
+    f = open(input_file, 'r')
+    ips = [line.rstrip() for line in f if line.rstrip()] # rm newlines
+    generate_iptables(output_file, ips)
+
 def generate_iptables(filename, ips):
     """
         Generate iptables files from templates.
     """
+    print("Generating iptables file:\n")
+    print("-----------")
     f = open('templates/%s' % filename)
     f = ''.join([line for line in f])
     t = Template(f)    
@@ -14,6 +21,9 @@ def generate_iptables(filename, ips):
 
     out = open('rendered/%s' % filename, 'w')
     out.write(rendered)
+    print(rendered)
+    print("-----------")
+    print("\nThe file was written to rendered/%s" % filename)
 
 if __name__ == "__main__":
-    generate_iptables('iptables.int.rules', ['1.1.1.1', '2.2.2.2'])
+    generate_iptables_from_file('iptables.int.rules', 'ips.txt')
